9acf16263bfd2fd19fa6d610ed082df424d72f5c
Ruan <sdias.ruan@gmail.com>
2021-10-18 00:08:11 -0300

    [Feature Dias - Task: User model]

--- src/main/java/org/generation/personalBlog/model/UserModel.java
+++ src/main/java/org/generation/personalBlog/model/UserModel.java
@@ -1,5 +1,44 @@
 package org.generation.personalBlog.model;
 
-public class UserModel {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table (name = "db_user")
+public class UserModel {
+	private @Id @GeneratedValue (strategy = GenerationType.IDENTITY) long id;
+	private @NotBlank String name;
+	private @NotBlank String user;
+	private @NotBlank @Size (min = 5, max = 100) String password;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
