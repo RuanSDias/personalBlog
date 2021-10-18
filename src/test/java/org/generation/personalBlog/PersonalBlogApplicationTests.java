6092d6c26e811af3c796fd08ebe8eb629b8ce140
Ruan <sdias.ruan@gmail.com>
2021-10-17 23:33:32 -0300

    [Feature Dias - Task: Importing Maven project]

---
+++ src/test/java/org/generation/personalBlog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.personalBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
