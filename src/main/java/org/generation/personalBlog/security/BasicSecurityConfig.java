0c783948a218458014c85a35ed1b785927e3a1cc
Ruan <sdias.ruan@gmail.com>
2021-11-04 14:41:30 -0300

    [Feature Dias - Task: bug]

--- src/main/java/org/generation/personalBlog/security/BasicSecurityConfig.java
+++ src/main/java/org/generation/personalBlog/security/BasicSecurityConfig.java
@@ -21,7 +21,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
 		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root"))
-				.authorities("ROLE_ADMIN");
+				.authorities("ROLE_USER");
 	}
 
 	@Bean
